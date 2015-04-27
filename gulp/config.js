7ffeff54e1edf273204c6a671974a4f717721db4
Skessler <stvnksslr@gmail.com>
2015-04-27 11:57:04 -0400

    refactor(app skeleton): filling out app skelton and proper split of tasks vs utils and as clean a gulp implementation as i can manage at this time

---
+++ gulp/config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+
+    'serverport': 3000,
+
+    'styles': {
+        'src': 'app/styles/**/*.scss',
+        'dest': 'build/css'
+    },
+
+    'scripts': {
+        'src': 'app/js/**/*.js',
+        'dest': 'build/js'
+    },
+
+    'images': {
+        'src': 'app/images/**/*',
+        'dest': 'build/images'
+    },
+
+    'views': {
+        'watch': [
+            'app/index.html',
+            'app/views/**/*.html'
+        ],
+        'src': 'app/views/**/*.html',
+        'dest': 'app/js'
+    },
+
+    'dist': {
+        'root': 'build'
+    },
+
+    'browserify': {
+        'entries': ['./app/js/main.js'],
+        'bundleName': 'main.js',
+        'sourcemap': true,
+    },
+
+    'test': {
+        'karma': 'test/karma.conf.js',
+        'protractor': 'test/protractor.conf.js'
+    }
+
+};
---
+++ gulp/tasks/babelify.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('default', function() {
+    gulp.src(['index.html'])
+        .pipe(gulp.dest('build/'));
+
+    gulp.src(['app/*.html', 'app/**/*.html', 'app/**/*.json'])
+        .pipe(gulp.dest('build/app'));
+
+    return browserify({
+            debug: true
+        })
+        .transform(babelify)
+        .require('./app/index.js', {
+            entry: true
+        })
+        .bundle()
+        .on('error', function handleError(err) {
+            console.error(err.toString());
+            this.emit('end');
+        })
+        .pipe(source('index.js'))
+        .pipe(gulp.dest('./build/app'));
+});
