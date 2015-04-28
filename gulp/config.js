84d801be86ae7a9b6bb6355b5419db10b79ae8de
Skessler <stvnksslr@gmail.com>
2015-04-28 17:20:50 -0400

    chore(linting)
    Added in a gulp task to run through the transpiled code with a linter
    and output any errors it finds.

--- gulp/config.js
+++ gulp/config.js
@@ -1,3 +1,16 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+
+    'serverport': 3000,
+
+    'scripts': {
+        'src': 'app/js/**/*.js',
+        'dest': 'build'
+    },
+
+    'dist': {
+        'root': 'build'
+    },
+
+};
