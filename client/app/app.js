fc194b4e780de388f51e077ce825833b4188c099
Steven Kessler <stvnksslr@gmail.com>
2017-03-23 16:54:50 -0400

    chore(code cleanup + removing docs): removed docs from being checked in by accident, docs should be generated when used.

--- client/app/app.js
+++ client/app/app.js
@@ -7,6 +7,7 @@ import mdTable from 'angular-material-data-table';
 
 import Common from './common/common';
 import Components from './components/components';
+
 // import AppComponent from './app.component';
 
 import 'normalize.css';
@@ -14,15 +15,20 @@ import 'angular-material/angular-material.css';
 import 'angular-material-data-table/dist/md-data-table.min.css';
 
 angular.module('app', [angularMaterial,
-        angularMessages,
-        angularAnimate,
-        mdTable,
-        uiRouter,
-        Common.name,
-        Components.name
-    ])
-    .constant('API_URL', '//localhost:3000')
-    .config(($locationProvider) => {
-        'ngInject';
-        $locationProvider.html5Mode(false);
-    });
+  angularMessages,
+  angularAnimate,
+  mdTable,
+  uiRouter,
+  Common.name,
+  Components.name
+])
+  .constant('API_URL', '//localhost:3000')
+
+  .config(($locationProvider) => {
+    'ngInject';
+
+    // remove ! hash prefix
+    $locationProvider.hashPrefix('');
+    // disable html 5 mode
+    $locationProvider.html5Mode(false);
+  });
