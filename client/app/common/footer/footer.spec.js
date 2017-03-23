807bff2656e205989f42095563544bd3323e272e
Steven Kessler <stvnksslr@gmail.com>
2017-03-23 16:49:23 -0400

    chore(fixing default test): fixing a default test that had a previous projects name still in the testcase.

--- client/app/common/footer/footer.spec.js
+++ client/app/common/footer/footer.spec.js
@@ -4,51 +4,51 @@ import FooterComponent from './footer.component';
 import FooterTemplate from './footer.html';
 
 describe('Footer', () => {
-    let $rootScope, makeController;
+  let $rootScope, makeController;
 
-    beforeEach(window.module(FooterModule.name));
+  beforeEach(window.module(FooterModule.name));
 
-    beforeEach(inject((_$rootScope_) => {
-        $rootScope = _$rootScope_;
-        makeController = () => {
-            return new FooterController();
-        };
-    }));
+  beforeEach(inject((_$rootScope_) => {
+    $rootScope = _$rootScope_;
+    makeController = () => {
+      return new FooterController();
+    };
+  }));
 
-    describe('Module', () => {
-        // top-level specs: i.e., routes, injection, naming
+  describe('Module', () => {
+    // top-level specs: i.e., routes, injection, naming
+  });
+
+  describe('Controller', () => {
+    // controller specs
+    it('has company name in controller', () => {
+      let controller = makeController();
+      expect(controller).to.have.property('name');
+    });
+  });
+
+  describe('Template', () => {
+    // template specs
+    // tip: use regex to ensure correct bindings are used e.g., {{  }}
+    it('has company name in the template', () => {
+      expect(FooterTemplate).to.match(/{{\s?vm\.name\s?}}/g);
+    });
+  });
+
+  describe('Component', () => {
+    // component/directive specs
+    let component = FooterComponent;
+
+    it('includes the intended template', () => {
+      expect(component.template).to.equal(FooterTemplate);
     });
 
-    describe('Controller', () => {
-        // controller specs
-        it('has zillion company name in controller', () => {
-            let controller = makeController();
-            expect(controller).to.have.property('name');
-        });
+    it('uses `controllerAs` syntax', () => {
+      expect(component).to.have.property('controllerAs');
     });
 
-    describe('Template', () => {
-        // template specs
-        // tip: use regex to ensure correct bindings are used e.g., {{  }}
-        it('has zillion company name in the template', () => {
-            expect(FooterTemplate).to.match(/{{\s?vm\.name\s?}}/g);
-        });
+    it('invokes the right controller', () => {
+      expect(component.controller).to.equal(FooterController);
     });
-
-    describe('Component', () => {
-        // component/directive specs
-        let component = FooterComponent;
-
-        it('includes the intended template', () => {
-            expect(component.template).to.equal(FooterTemplate);
-        });
-
-        it('uses `controllerAs` syntax', () => {
-            expect(component).to.have.property('controllerAs');
-        });
-
-        it('invokes the right controller', () => {
-            expect(component.controller).to.equal(FooterController);
-        });
-    });
+  });
 });
